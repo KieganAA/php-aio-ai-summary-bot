426b6f1fc4f41342088b17e00dd6c33c2c4c7b9e
KieganAA <91825582+KieganAA@users.noreply.github.com>
2025-08-05 22:07:36 +0300

    feat: aggregate daily reports into digest

---
+++ src/Console/DailyDigestCommand.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace Src\Console;
+
+use Psr\Log\LoggerInterface;
+use Src\Service\ReportService;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class DailyDigestCommand extends Command
+{
+    protected static $defaultName = 'app:daily-digest';
+    protected static $defaultDescription = 'Generate daily report of reports';
+
+    public function __construct(private ReportService $report, private LoggerInterface $logger)
+    {
+        parent::__construct(self::$defaultName);
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $this->logger->info('Daily digest command started');
+        $this->report->runDigest(time());
+        $this->logger->info('Daily digest command finished');
+        return Command::SUCCESS;
+    }
+}
+
